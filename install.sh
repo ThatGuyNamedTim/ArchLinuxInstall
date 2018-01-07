7eb42beefa1fe07065f2b2dbe44bf25b0ab03cfd
ThatGuyNamedTim <tim.j.thoresen@gmail.com>
2018-01-07 13:32:55 -0500

    More personalization

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # This will install Arch Linux for an EFI system with gdm and the gnome desktop
 # environment
@@ -45,20 +45,8 @@ done
 read -p "Do you want to set up github (y/n): " githubChoice
 if [ "$githubChoice" == "y" ] || [ "$githubChoice" == "Y" ]
 then
-  read -p "github username: " githubuser
-  read -p "github password: " -s githubpass1
-  echo
-  read -p "Re-enter github password: " -s githubpass2
-  echo
-
-  while [ "$githubpass1" != "$githubpass2" ]
-  do
-    printf "Passwords do not match, please try again\n"
-    read -p "github password: " -s githubpass1
-    echo
-    read -p "Re-enter github password: " -s githubpass2
-    echo
-  done
+  read -p "github username: " githubUsername
+  read -p "github email: " githubEmail
 fi
 
 # Instalation #################################################################
@@ -151,19 +139,16 @@ rankmirrors -n 10 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist
 # Generate fstab for system configuration #######
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Enter the installation #######
-arch-chroot /mnt
+# Run a script in the installation #######
+# First create the script file with echo commands then make it excecutable
 
-# Timing fixes #######
-# Set the time zone
-
-# Personalize #################################################################
-
-# install software #######
-
-# pacman -Syu  atom cronie curl dconf dconf-editor flashplugin gcc gdm gimp git gnome \
-#  gnome-tweak-tool grep libreoffice mono ntp ocaml perl pip powertop \
-#  python ruby sshd unzip vim vlc wget
+# Downloads
+echo '#!/bin/sh
+pacman -Syu  atom cronie curl dconf dconf-editor flashplugin \
+gcc gdm gimp git gnome \
+gnome-tweak-tool grep libreoffice mono ntp ocaml perl pip powertop \
+python ruby sshd unzip vim virtualbox virtualbox-guest-utils vlc \
+wget' > /mnt/installScript.sh
 
 # atom - text editor
 # cronie - used for crone jobs
@@ -190,9 +175,92 @@ arch-chroot /mnt
 # sshd - used for secure shell
 # unzip - command to unzip
 # vim - text editor
-# vlc - media plater
+# virtualbox - a virtual machine tool
+# virtualbox - guest-utils - a tool for virtual machines
+# vlc - media player
 # wget - tool to download
 
+# Install packages from the AUR
+echo "
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -sic
+cd ..
+rm -rf yaourt
+yaourt google-chrome
+yaourt papirus-icon-theme-git
+yaourt papirus-folders-git
+papirus-folders -C black
+yaourt gtk-theme-arc-grey
+yaourt ttf-ms-fonts
+
+yaourt capitaine-cursors
+sudo cp /usr/share/icons/capitaine-cursors/cursors/dnd-move \
+/usr/share/icons/capitaine-cursors/cursors/fleur
+sudo rm -f /usr/share/icons/capitaine-cursors/cursors/size_bdiag
+sudo rm -f /usr/share/icons/capitaine-cursors/cursors/size_fdiag
+sudo rm -f /usr/share/icons/capitaine-cursors/cursors/size_hor
+sudo rm -f /usr/share/icons/capitaine-cursors/cursors/size_ver
+" >> /mnt/personalize
+# Yaourt is a manager for the AUR
+# Google is a browser
+# Papirus have the best icons
+# Papirus-folders allows one to change the folder color
+# arc-grey gnome theme
+# Font for microsoft
+# capitaine is a curosor
+# edit the cursor icons to look better
+
+# Create icons for the desktop
+echo " ln -s ~/Pictures ~/Desktop/Pictures
+gio set ~/Desktop/Pictures metadata::custom-icon \
+file:///usr/share/icons/Papirus/48x48/places/folder-black-pictures.svg
+
+ln -s ~/Documents ~/Desktop/Documents
+gio set ~/Desktop/Documents metadata::custom-icon \
+file:///usr/share/icons/Papirus/48x48/places/folder-black-documents.svg
+
+ln -s ~/Downloads ~/Desktop/Downloads
+gio set ~/Desktop/Downloads metadata::custom-icon \
+file:///usr/share/icons/Papirus/48x48/places/folder-black-download.svg
+
+gsettings set org.gnome.nautilus.icon-view default-zoom-level 'standard'" > /mnt/personalize
+
+# Set up github
+
+if [ "$githubChoice" == "y" ] || [ "$githubChoice" == "Y" ]
+then
+  echo "git config --global user.name $githubUsername" >> /mnt/personalize
+  echo "git config --global user.email $githubEmail" >> /mnt/personalize
+done
+
+# Create the .vimrc
+echo "echo 'command W w \"allow caps
+command Wq wq \"allow caps
+command WQ wq \"allow caps
+command Q q \"allow caps
+
+syntax on \"nice coloring of syntax
+set number \"adds line numbers
+set expandtab \"spaces for tabs
+set sw=4 \"default to tabstop
+set tabstop=4 \"visual spaces per tab
+set softtabstop=4 \"tab spaces while editing
+set wildmenu \"visual autocomplete
+set incsearch \"search as characters are entered
+set hlsearch \"highlight matches
+set backspace=indent,eol,start \"allows use of backspace
+set autoindent \"new lines inherit the indent from the previous lines
+set history=50 \"improves the undo property
+set background=light
+set termguicolors
+colorscheme PaperColor
+set background=dark' > /home/tim/.vimrc" > /mnt/personalize
+
+# Run the script to personalize the installation
+arch-chroot /mnt /mnt/personalize
+
+# Remove the personalize script
+rm /mnt/personalize
+
 # Enable on start up
-sudo systemctl daemon-reload
-systemctl enable ntpd
